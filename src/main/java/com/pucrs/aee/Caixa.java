e32f543b77a88313f413824e31e999448813aad8
Homero Junior <homero_junior_oliveira@outlook.com>
2016-10-04 01:15:32 -0300

    Implementando métodos da classe Caixa falta implementar o método atender novo cliente.

--- src/main/java/com/pucrs/aee/Caixa.java
+++ src/main/java/com/pucrs/aee/Caixa.java
@@ -4,4 +4,37 @@ package com.pucrs.aee;
  * Created by Junior on 04/10/2016.
  */
 public class Caixa {
+
+    private int numeroDeAtendidos;
+    private Cliente clienteAtual;
+
+    public Caixa(){
+        numeroDeAtendidos = 0;
+        clienteAtual = null;
+    }
+
+    public void atenderNovoCliente(Cliente cliente){
+        // TODO: 04/10/2016 Implementar este método.
+    }
+
+    public Cliente dispensarClienteAtual(){
+        if(clienteAtual == null){
+            throw new RuntimeException("Não existe nenhum cliente para dispensar.");
+        }
+        Cliente cliente = this.clienteAtual;
+        this.clienteAtual = null;
+        return cliente;
+    }
+
+    public boolean isVazio(){
+        return clienteAtual == null;
+    }
+
+    public Cliente getClienteAtual() {
+        return clienteAtual;
+    }
+
+    public int getNumeroDeAtendidos() {
+        return numeroDeAtendidos;
+    }
 }
